87fb040f4fed9d53c4ba44758f79a997946ec84f
Thuy Le <ngocthuyle19@gmail.com>
2024-08-08 08:20:19 -0400

    RFM Metrics

--- Ecommerce_script.R
+++ Ecommerce_script.R
@@ -14,9 +14,7 @@ library(tidyr)
 ## 1.2 Download and Load Data
 install.packages("readxl")
 library(readxl)
-getwd()
-file.exists('/Users/moon/R Project Ecommerce/online_retail_II.xlsx')
-retail_data <- read_excel('online_retail_II.xlsx', sheet='Year 2009-2010')
+retail_data <- read_excel('./data/online_retail_II.xlsx', sheet='Year 2009-2010')
 head(retail_data,5)
 
 ## 1.3 Explore the Dataset
@@ -30,11 +28,6 @@ str(retail_df)
 
 library(skimr) # skim function display the numerical attributes from summary, missing values, quantile information and histogram
 skim(retail_df)
-
-### Create report in Data Explorer
-install.packages('DataExplorer')
-library(DataExplorer)
-DataExplorer::create_report(retail_df)
 
 
 # II. CLEAN DATA
@@ -68,6 +61,7 @@ top_products <- retail_df %>%
   summarise(Total_Revenue=sum(Amount)) %>%
   arrange(desc(Total_Revenue)) %>%
   head(10)
+
 print(top_products)
 
 ggplot(top_products, aes(x=reorder(Description, Total_Revenue), y=Total_Revenue)) +
@@ -102,16 +96,15 @@ ggplot(revenue_by_country, aes(x=reorder(Country, Total_Revenue), y=Total_Revenu
   theme_gray()
 
 # IV. DATA PREPARATION
+## 4.1 RFM Metrics 
 #      We are going to analysis the Customers based on below 3 factors:
 #           R (Recency): Number of days since last purchase
 #           F (Frequency): Number of transactions
 #           M (Monetary): Total amount of transactions (revenue contributed)
 
 # Add new attribute: Monetary
-library(dplyr)
-retail_df$Amount <- retail_df$Quantity * retail_df$Price
 retail_grp_monetary <- retail_df %>% group_by(Customer_ID) %>%
-                              summarise(total_amount = sum(Amount))
+                              summarise(total_revenue = sum(Amount))
 retail_grp_monetary
 
 # Add new attribute: Frequency
@@ -128,13 +121,88 @@ max_date
 retail_df$Diff <- (max_date - retail_df$Invoice_Date)
 
 retail_grp_recency <- retail_df %>% group_by(Customer_ID) %>% 
-                      summarize(Min_Recency = min(Diff, na.rm = TRUE))
-retail_grp_recency$Min_Recency <- as.numeric(retail_grp_recency$Min_Recency, units = "days")
+                      summarize(min_recency = min(Diff, na.rm = TRUE))
+retail_grp_recency$min_recency <- as.numeric(retail_grp_recency$min_recency, units = "days")
 retail_grp_recency
 
 # Merge the attributes to get the RFM data frame
-frm_df <- retail_grp_monetary %>%
+rfm_df <- retail_grp_monetary %>%
   left_join(retail_grp_freq, by = "Customer_ID") %>%
   left_join(retail_grp_recency, by = "Customer_ID")
-frm_df
+rfm_df
 
+# 4.2 Create RFM score
+rfm_df$total_revenue_score <- cut(rfm_df$total_revenue, 
+                                  breaks=quantile(rfm_df$total_revenue, probs=seq(0,1,0.2), na.rm=TRUE),
+                                  include.lowest = TRUE,
+                                  labels=c(1,2,3,4,5))
+rfm_df$frequency_score <- cut(rfm_df$frequency, 
+                                  breaks=quantile(rfm_df$frequency, probs=seq(0,1,0.2), na.rm=TRUE),
+                                  include.lowest = TRUE,
+                                  labels=c(1,2,3,4,5))
+rfm_df$recency_score <- cut(rfm_df$min_recency, 
+                              breaks=quantile(rfm_df$min_recency, probs=seq(0,1,0.2), na.rm=TRUE),
+                              include.lowest = TRUE,
+                            labels=c(5,4,3,2,1))
+rfm_df$rfm_score <- paste0(
+  as.character(rfm_df$recency_score),
+  as.character(rfm_df$frequency_score)
+)
+
+head(rfm_df,5)
+# Function to map RFM_SCORE to segments
+seg_map <- c(
+  'Hibernating' = '^[1-2][1-2]$',
+  'At Risk' = '^[1-2][3-4]$',
+  "Can't Lose" = '^[1-2]5$',
+  'About to Sleep' = '^3[1-2]$',
+  'Need Attention' = '^33$',
+  'Loyal Customers' = '^[3-4][4-5]$',
+  'Promising' = '^41$',
+  'New Customers' = '^51$',
+  'Potential Loyalists' = '^[4-5][2-3]$',
+  'Champions' = '^5[4-5]$'
+)
+
+
+rfm_df$Segment <- sapply(rfm_df$rfm_score, segment_map)
+head(rfm_df)
+
+
+# Group by Segment and calculate statistics
+rfmStats <- rfm_df %>%
+  group_by(Segment) %>%
+  summarise(
+    Recency_mean = mean(min_recency, na.rm = TRUE),
+    Recency_median = median(min_recency, na.rm = TRUE),
+    Recency_count = n(),
+    Recency_std = sd(min_recency, na.rm = TRUE),
+    Frequency_mean = mean(frequency, na.rm = TRUE),
+    Frequency_median = median(frequency, na.rm = TRUE),
+    Frequency_count = n(),
+    Frequency_std = sd(frequency, na.rm = TRUE),
+    Monetary_mean = mean(total_revenue, na.rm = TRUE),
+    Monetary_median = median(total_revenue, na.rm = TRUE),
+    Monetary_count = n(),
+    Monetary_std = sd(total_revenue, na.rm = TRUE)
+  )
+
+head(rfmStats)
+
+# Visualize Treemap
+install.packages("treemap")
+library(treemap)
+segment_summary <- rfm_df %>%
+  group_by(Segment) %>%
+  summarise(Customer_Count = n())
+
+segment_summary
+
+ggplot(segment_summary, aes(x=reorder(Segment, Customer_Count), y=Customer_Count)) +
+  geom_col(fill = 'blue', color = 'black') +
+  coord_flip()
+  labs(title = 'Number of customers by segments',
+       x = 'Segment',
+       y= 'Number of customers') +
+  theme_gray()
+  
